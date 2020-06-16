de2aadd837e8cd7ebc5d9335c76766fe42b82ec4
Debolin <46442437+debolindhar@users.noreply.github.com>
2020-06-16 19:43:59 +0530

    Update create_table_link.sql

--- src/main/resources/issue-8534/create_table_link.sql
+++ src/main/resources/issue-8534/create_table_link.sql
@@ -1,11 +1,7 @@
 --liquibase formatted sql
 --changeset debolin:20200609012500 
 
-CREATE TABLE link (
-   ID serial PRIMARY KEY,
-   url VARCHAR (255) NOT NULL,
-   name VARCHAR (255) NOT NULL,
-   description VARCHAR (255),
-   rel VARCHAR (50)
-);
+CREATE TABLE Student (
+StudentName char(20),
+StudentRoll int);
 --rollback drop table link
